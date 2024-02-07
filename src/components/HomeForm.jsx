ffec5fa5a8034bde2bd754cddcb22de22fb60d44
Theo Marentes <theokazmaros@gmail.com>
2024-02-08 09:08:31 +1000

    updated home form

--- src/components/HomeForm.jsx
+++ src/components/HomeForm.jsx
@@ -1,27 +1,54 @@
-import React from 'react';
-import './styles/HomeTrackerForm.css'
+import React, {   useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './styles/HomeTrackerForm.css';
+import './styles/TrackerForm.css';
+
 
 const HomeForm = () => {
+  const [username, setUsername] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleInputChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleSearch = (event) => {
+    event.preventDefault(); // Prevent the default form submission
+    
+    navigate(`/tracker/${username}`);
+
+    
+    
+    
+  };
+
+
+
   return (
-      <div className="content text-light">
-        <div className="centered-content">
-          <h1 className="display-4 heading-yellow">Theoatrix Toolkit</h1>
-          <h2 className="lead">Master OldSchool RuneScape</h2>
-          <form>
-            <div className="input-group mb-3">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter Your RuneScape Name"
-              />
-              <div className="input-group-append">
-                <button className="btn btn-primary" type="button">
-                  Search
-                </button>
-              </div>
+    <div style={{paddingTop:"10vh", paddingBottom: "10vh"}}class="content">
+      <div className="centered-content">
+        <h1 className="display-4 heading-yellow">Theoatrix Toolkit</h1>
+        <h2 className="lead">Master Old School RuneScape</h2>
+        <form onSubmit={handleSearch}>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Your RuneScape Name"
+              value={username}
+              onChange={handleInputChange}
+            />
+            <div className="input-group-append">
+              <button className="btn btn-primary" type="submit">
+                Search
+              </button>
+              
             </div>
-          </form>
-        </div>
+            
+          </div>
+             </form>
+      </div>
       </div>
   );
 };
