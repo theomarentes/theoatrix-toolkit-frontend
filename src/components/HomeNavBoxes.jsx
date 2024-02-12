2c5e3f76e97e0edfc0e6b4aa6ee567ac221fd44a
Theo Marentes <theokazmaros@gmail.com>
2024-02-12 10:30:34 +1000

    updated home nav boxes to include simulator

--- src/components/HomeNavBoxes.jsx
+++ src/components/HomeNavBoxes.jsx
@@ -11,10 +11,7 @@ const HomeNavBoxes = ({ children }) => {
         <img src={require("../files/Skills-icon.png")} className="icon" alt="Skills icon" />
         <p>Account Tracker</p>
       </a>
-      <a href="/guides" className="box">
-        <img src={require("../files/Construction-guide.png")} className="icon" alt="Construction guide" />
-        <p>1-99 Guides</p>
-      </a>
+     
       <a href="/time-to-max" className="box">
         <img src={require("../files/Max_cape.png")} className="icon" alt="Max cape" />
         <p>Time To Max</p>
@@ -23,9 +20,17 @@ const HomeNavBoxes = ({ children }) => {
         <img src={require("../files/High-Alchemy.png")} className="icon" alt="High Alchemy" />
         <p>High Alchemy</p>
       </a>
+      <a href="/simulator" className="box">
+        <img src={require("../files/Olmlet.webp")} className="icon" alt="High Alchemy" />
+        <p>Drop Simulator</p>
+      </a>
       <a href="/grand-exchange" className="box">
         <img src={require("../files/coins.png")} className="icon" alt="Coins" />
         <p>Grand Exchange</p>
+      </a>
+      <a href="/guides" className="box">
+        <img src={require("../files/Construction-guide.png")} className="icon" alt="Construction guide" />
+        <p>1-99 Guides</p>
       </a>
       <a href="/my-account" className="box">
         <img src={require("../files/junk-checker.png")} className="icon" alt="Junk checker" />
